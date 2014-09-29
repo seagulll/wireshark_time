617933f9e571cd410b43b8de33e00068914793e6
seagulll <ling.d.yu@gmail.com>
2014-09-29 15:49:50 +0800

    First version.

---
+++ src/read_time.py
@@ -0,0 +1,409 @@
+'''
+Created on Jun 9, 2014
+
+@author: elingyu
+'''
+
+#! /usr/bin/python
+
+
+import optparse
+
+from datetime import datetime
+
+def parse_args():
+    usage = """usage: %prog [options]
+This is the wireshark file handler.
+Run it like this:
+
+    python read_time.py -d "C:\simon\Company\SBG\Test_result\R14B\Logs\SBG-SCT-4401.2\" -a "access.txt" -c "core.txt" -r "res.txt"  
+    
+"""
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-d", "--directory", dest="directory", type="string", help="access network", default="c:")
+    parser.add_option("-a", "--access", dest="access", type="string", help="access network", default="access.txt")
+    parser.add_option("-c", "--core", dest="core", type="string", help="core network", default="core.txt")
+    parser.add_option("-r", "--result", dest="result", type="string", help="result file", default="res.txt")
+
+    (options, _) = parser.parse_args()
+
+    print "The directory is %s" % (options.directory)
+
+    print "The access network file is %s" %(options.access)
+    
+    print "The core network file is %s" %(options.core)
+    
+    print "The result will be put in the file %s" %(options.result)
+    
+    return options.directory, options.access, options.core, options.result
+
+
+def cal_reg(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: REGISTER" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: REGISTER" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["REGISTER (init) Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close()
+    
+
+def cal_inv(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: INVITE" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: INVITE" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["INVITE Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close()
+    
+
+def cal_rin(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Status: 180 Ringing" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Status: 180 Ringing" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["180 Ringing Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close()
+
+
+def cal_ok(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Status: 200 OK, with session description" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Status: 200 OK, with session description" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["200 OK Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close()
+    
+
+def cal_ack(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: ACK" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: ACK" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["ACK Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close()
+
+
+def cal_bye(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: BYE" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: BYE" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["BYE Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close() 
+
+
+def cal_sub(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: SUBSCRIBE" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: SUBSCRIBE" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["SUBSCRIBE Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close() 
+
+
+def cal_pub(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: PUBLISH" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: PUBLISH" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["PUBLISH Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close() 
+
+
+def cal_mes(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: MESSAGE" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: MESSAGE" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["MESSAGE Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close() 
+    
+
+def cal_not(directory, access, core, result):
+    access_file = open(directory + "//" + access, 'r')
+    access_time = []
+    for line in access_file:
+        if "Request: NOTIFY" in line:
+            access_time.append(line.split()[1])
+    if len(access_time) == 0:
+        return
+    print access_time
+    access_file.close()
+
+    core_file = open(directory + "//" + core, 'r')
+    core_time = []
+    for line in core_file:
+        if "Request: NOTIFY" in line:
+            core_time.append(line.split()[1])
+    print core_time
+    core_file.close()
+
+    access_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in access_time]
+    core_time = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(access_time, core_time)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["NOTIFY Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close() 
+    
+
+def cal_dia(directory, access, core, result):
+    core_file = open(directory + "//" + core, 'r')
+    core_time_1 = []
+    for line in core_file:
+        if "cmd=AARequest" in line:
+            core_time_1.append(line.split()[1])
+    if len(core_time_1) == 0:
+        return
+    print core_time_1
+    core_file.close()
+    
+    core_file = open(directory + "//" + core, 'r')
+    core_time_2 = []
+    for line in core_file:
+        if "cmd=AAAnswer" in line:
+            core_time_2.append(line.split()[1])
+    print core_time_2
+    core_file.close()
+
+    core_time_1 = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time_1]
+    core_time_2 = [datetime.strptime(r, '%H:%M:%S.%f') for r in core_time_2]
+    diff = [abs((a - c).total_seconds()) for a,c in zip(core_time_1, core_time_2)]
+    print diff
+    
+    ave = sum(diff) / len(diff)
+    print ave
+    
+    result_file = open(directory + "//" + result, 'a')
+    result_file.writelines(str(i)+"\n" for i in diff + ["DIAMETER Average delay: " + str(ave) + " secs" + "\n"])
+    result_file.close() 
+    
+
+if __name__ == '__main__':
+    parameters = parse_args()
+    cal_reg(*parameters)
+    cal_inv(*parameters)
+    cal_rin(*parameters)
+    cal_ok(*parameters)
+    cal_ack(*parameters)
+    cal_bye(*parameters)
+    cal_sub(*parameters)
+    cal_pub(*parameters)
+    cal_mes(*parameters)
+    cal_not(*parameters)
+    cal_dia(*parameters)
+    
+    print "Done!"
+
+
